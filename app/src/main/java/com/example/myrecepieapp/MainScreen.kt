b63c1ab6efb2dfd65f61f3260af04c85c3783f3b
harshk2003 <harshkoul2003@gmail.com>
2025-03-27 12:46:55 +0530

    RecipeApp Project

--- app/src/main/java/com/example/myrecepieapp/MainScreen.kt
+++ app/src/main/java/com/example/myrecepieapp/MainScreen.kt
@@ -1,6 +1,6 @@
 package com.example.myrecepieapp
 
-import android.util.Log
+
 import androidx.compose.animation.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
@@ -12,10 +12,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
+
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
+
 
 @Composable
 fun MainScreen(viewModel: MainViewModel, onNext: () -> Unit) {
